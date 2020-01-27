022d8c32214f025a18c4661087a88f2b58e86f18
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-26 21:00:24 -0800

    create ability to parse and report on csv data

--- build/index.js
+++ build/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log('hi');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var matches = fs_1.default
+    .readFileSync('football.csv', {
+    encoding: 'utf-8' //utf8 is a string of data
+})
+    .split('\n')
+    .map(function (row) {
+    return row.split(',');
+});
+var manunitedWins = 0;
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manunitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === 'A') {
+        manunitedWins++;
+    }
+}
+console.log("Man United won " + manunitedWins + " games");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hi')
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8' //utf8 is a string of data
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+let manunitedWins = 0;
+
+for (let match of matches){
+    if(match[1] === 'Man United' && match[5] ==='H'){
+        manunitedWins++;
+    } else if (match[2] === 'Man United' && match[5] ==='A'){
+        manunitedWins++;
+    }
+    
+}
+
+console.log(`Man United won ${manunitedWins} games`)
