14a37b0beb34e69ce4a10e05590412e2d3265df7
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-27 22:27:11 -0800

    start refactor

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -17,19 +17,19 @@ export class CsvFileReader {
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
-      }).map((row: string[]): MatchData => {
-          //after row is split we map again to parse out values of the row
-        return [
-            //data within the row is in a standard format so we can run some processing on the original values and return a new array
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            //type assertion. TS does not know what is in this array but we do so we tell ts what type is expected
-            row[5] as MatchResult,
-            row[6]
-        ]
-      })
+      }).map(this.mapRow)
+  }
+  mapRow(row: string[]): MatchData{
+    return [
+      //data within the row is in a standard format so we can run some processing on the original values and return a new array
+      dateStringToDate(row[0]),
+      row[1],
+      row[2],
+      parseInt(row[3]),
+      parseInt(row[4]),
+      //type assertion. TS does not know what is in this array but we do so we tell ts what type is expected
+      row[5] as MatchResult,
+      row[6]
+  ]
   }
 }
