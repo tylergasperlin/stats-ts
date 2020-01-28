fe1fc518c4f68279f2936b7800afde2f7c5a1c1f
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-27 22:36:05 -0800

    continue refactor

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,13 +1,15 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils';
 import {MatchResult} from './Enums'
 
 type MatchData = [Date, string, string, number, number, MatchResult, string]
 
-export class CsvFileReader {
+export abstract class CsvFileReader {
   data: MatchData[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): MatchData;
+
 
   read(): void {
     this.data = fs
@@ -18,18 +20,5 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       }).map(this.mapRow)
-  }
-  mapRow(row: string[]): MatchData{
-    return [
-      //data within the row is in a standard format so we can run some processing on the original values and return a new array
-      dateStringToDate(row[0]),
-      row[1],
-      row[2],
-      parseInt(row[3]),
-      parseInt(row[4]),
-      //type assertion. TS does not know what is in this array but we do so we tell ts what type is expected
-      row[5] as MatchResult,
-      row[6]
-  ]
   }
 }
---
+++ src/MatchReader.ts
@@ -0,0 +1,20 @@
+import {CsvFileReader} from './CsvFileReader'
+import { dateStringToDate } from './utils';
+import {MatchResult} from './Enums'
+
+
+export class MatchReader extends CsvFileReader{
+    mapRow(row: string[]): MatchReader{
+        return [
+            //data within the row is in a standard format so we can run some processing on the original values and return a new array
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            //type assertion. TS does not know what is in this array but we do so we tell ts what type is expected
+            row[5] as MatchResult,
+            row[6]
+        ]
+    }
+}
