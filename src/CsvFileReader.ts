7e19f1f830f224fe1b4774a6d9b1601611e79e09
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-28 20:49:59 -0800

    add note

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -3,6 +3,8 @@ import fs from 'fs';
 
 //T is an argument for generic types 
 //T can be anything we want but by convention we use T
+//note that we use an abstract class and a generic at the same time
+//abstract class ensures that we implement on the initializing of csvfilereader
 export abstract class CsvFileReader<T> {
   data: T[] = [];
 
