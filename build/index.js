55430df8850592291bda668fd2bb28fe9918948b
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-29 07:04:02 -0800

    add html report

--- build/index.js
+++ build/index.js
@@ -5,10 +5,13 @@ var CsvFileReader_1 = require("./CsvFileReader");
 var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
 var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 var Summary_1 = require("./Summary");
+var htmlReport_1 = require("./reportTargets/htmlReport");
 //create object that satisfies datareader interface
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 //create instance of matchreader and pass in something satifying datareader internace
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
 var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+var htmlSummary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new htmlReport_1.HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
+htmlSummary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {CsvFileReader} from './CsvFileReader'
 import {ConsoleReport} from './reportTargets/ConsoleReport'
 import {WinsAnalysis} from './analyzers/WinsAnalysis'
 import {Summary} from './Summary'
-
+import {HtmlReport} from './reportTargets/htmlReport'
 //create object that satisfies datareader interface
 const csvFileReader = new CsvFileReader('football.csv')
 //create instance of matchreader and pass in something satifying datareader internace
@@ -15,5 +15,11 @@ const summary = new Summary(
   new ConsoleReport()
 )
 
+const htmlSummary = new Summary(
+  new WinsAnalysis('Man United'),
+  new HtmlReport()
+)
+
 summary.buildAndPrintReport(matchReader.matches)
+htmlSummary.buildAndPrintReport(matchReader.matches)
 
