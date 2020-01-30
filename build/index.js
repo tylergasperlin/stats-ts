65c525b017b3c625ac969eaf082c3ee3ef20e8d6
Tyler Gasperlin <gasperlint@gmail.com>
2020-01-29 19:41:06 -0800

    add notes

--- build/index.js
+++ build/index.js
@@ -4,9 +4,12 @@ var MatchReader_1 = require("./MatchReader");
 var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
 var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 var Summary_1 = require("./Summary");
+//matchreader
 var matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
 matchReader.load();
+//html report using a static method
 var htmlSummary = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
 htmlSummary.buildAndPrintReport(matchReader.matches);
+//console report
 var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
 summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,15 @@ import {WinsAnalysis} from './analyzers/WinsAnalysis'
 import {Summary} from './Summary'
 import {HtmlReport} from './reportTargets/htmlReport'
 
-
+//matchreader
 const matchReader = MatchReader.fromCsv('football.csv')
 matchReader.load()
 
+//html report using a static method
 const htmlSummary = Summary.winsAnalysisWithHtmlReport('Man United')
 htmlSummary.buildAndPrintReport(matchReader.matches)
 
-
-
+//console report
 const summary = new Summary(
   new WinsAnalysis('Man United'),
   new ConsoleReport()
